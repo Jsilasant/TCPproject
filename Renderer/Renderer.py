e688a48f4e436e8cab702e4a199dc4c67fdcd137
Jethjera Silasant <36745524+Jsilasant@users.noreply.github.com>
2018-12-02 16:07:21 -0600

    Nothing really changed
    Still only prints lines when needed.

--- Renderer/Renderer.py
+++ Renderer/Renderer.py
@@ -4,19 +4,19 @@ import os
 
 serverName = "localhost"
 serverPort = 48998
+#RenderPort = 48995
 RenderSocket = socket(AF_INET, SOCK_STREAM)
+#ClientRecv = socket(AF_INET, SOCK_STREAM)
+#ClientRecv.bind((serverName, RenderPort))
+#ClientRecv.listen(1)
 
 RenderSocket.connect((serverName, 48996))
 print('Renderer has connected!')
 while 1:
-    print('Im in the while loop')
+    #input = RenderSocket.recv(1024)
     lines = RenderSocket.recv(1024)
     lines = lines.decode('utf-8')
     print(lines)
-    #if( lines == True):
-        #print(lines)
+   # if (input == 'pause'):
+    #    print('im trying to pause')
 
-        
-    #elif( lines == False):
-        #print('Cannot read file! Please request a different one.')
-        
